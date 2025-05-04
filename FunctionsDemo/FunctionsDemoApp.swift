ad7f9eb35ac3491d9a14ba8c281703171db04c44
Jeremy <jeremy.skrdlant@nwktc.edu>
2025-05-04 09:28:32 -0600

    Initial Commit

---
+++ FunctionsDemo/FunctionsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunctionsDemoApp.swift
+//  FunctionsDemo
+//
+//  Created by admin on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FunctionsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
